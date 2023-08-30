4b04782066b0098a98759113def3594579781043
bohdanbatsenko <bohdan.batsenko.vaimo.com>
2023-08-30 10:11:22 +0300

    Popup code fixed

--- add-cart-popup.js
+++ add-cart-popup.js
@@ -12,19 +12,19 @@ define([
     $.widget('namespace.widgetName', {
         options: {
             freeShippingThreshold: 100,
-            messageFreeShipping: 'Your order now have free shipping',
+            messageFreeShipping: $t('Your order now have free shipping'),
             modalOptions: {
                 modalClass: 'add-to-cart-popup',
                 title: $.mage.__("Popup Title"),
                 buttons: [
                     {
-                        text: 'Continue Shopping',
+                        text: $t('Continue Shopping'),
                         click: function () {
                             this.closeModal();
                         }
                     },
                     {
-                        text: 'Proceed to Checkout',
+                        text: $t('Proceed to Checkout'),
                         click: function () {
                             window.location = window.checkout.checkoutUrl
                         }
@@ -78,7 +78,7 @@ define([
 
         callPopup(){
             setTimeout(()=> {
-                customerData.reload(['cart'], true);
+                //customerData.reload(['cart'], true);
 
                 const customer = customerData.get('customer')();
                 const cart = customerData.get('cart')();
@@ -107,6 +107,7 @@ define([
                     <p>${$t('Items in cart: %1').replace('%1', cart.summary_count)}</p>
                     <p>${$t('Cart subtotal: %1').replace('%1', cart.subtotal)}</p>
                   `;
+
                 } else if (cart.subtotalAmount >= this.options.freeShippingThreshold) {
                     popupContent = `
                     <div class="progress-wrapper">
@@ -124,73 +125,6 @@ define([
                 }
 
                 popupWrapper.html(popupContent).modal(this.options.modalOptions).modal('openModal');
-                //popupWrapper.;
-
-
-                 // if (formattedTotal === '0' ) {
-                    //     //popup modal code
-                    //     let popup = $('<div class="add-to-cart-modal-popup"/>')
-                    //         .html(
-                    //             `
-                    //                 <p>${$t('Hi, %1').replace('%1', customer.firstname)}</p>
-                    //                 <p>${$t('Items in cart: %1').replace('%1', cart.summary_count)}</p>
-                    //                 <p>${$t('Cart subtotal: %1').replace('%1', Number(cart.subtotalAmount).toFixed(0))}</p>
-                    // `
-                    //         )`
-                    //         .modal({
-                    //             modalClass: 'add-to-cart-popup',
-                    //             title: $.mage.__("Popup Title"),
-                    //             buttons: [
-                    //                 {
-                    //                     text: 'Continue Shopping',
-                    //                     click: function () {
-                    //                         this.closeModal();
-                    //                     }
-                    //                 }
-                    //             ]
-                    //         });
-                    //     popup.modal('openModal');
-                    // }
-                    // else if (cart.subtotalAmount > 0 && cart.subtotalAmount < this.options.freeShippingThreshold) {
-                    //     const subtotalRemaining = this.options.freeShippingThreshold - cart.subtotalAmount;
-                    //     let popupWithProgress = $('<div class="add-to-cart-modal-popup"/>')
-                    //         .html(`
-                    //             <div class="progress-wrapper">
-                    //             <h2>Heyhey</h2>
-                    //                 <div class="progress-bar">
-                    //                     <span class="progress-bar-fill" style="width: ${formattedTotal}%;">
-                    //                         <span class="percent-bar">${formattedTotal}%</span>
-                    //                     </span>
-                    //                 </div>
-                    //             </div>
-                    //             <p>${$t('Hi, %1').replace('%1', customer.firstname)}</p>
-                    //             <p>You are ${subtotalRemaining}$ away from FREE shipping</p>
-                    //             <p>${$t('Items in cart: %1').replace('%1', cart.summary_count)}</p>
-                    //             <p>${$t('Cart subtotal: %1').replace('%1', cart.subtotal)}</p>
-                    //             `
-                    //         )
-                    //         .modal({
-                    //             modalClass: 'add-to-cart-popup',
-                    //             title: $.mage.__("Popup Title"),
-                    //             buttons: [
-                    //                 {
-                    //                     text: 'Continue Shopping',
-                    //                     click: function () {
-                    //                         this.closeModal();
-                    //                     }
-                    //                 },
-                    //                 {
-                    //                     text: 'Proceed to Checkout',
-                    //                     click: function () {
-                    //                         window.location = window.checkout.checkoutUrl
-                    //                     }
-                    //                 }
-                    //             ]
-                    //         });
-                    //     popupWithProgress.modal('openModal');
-                    // }
-
-
             }, 1800);
         },
 
