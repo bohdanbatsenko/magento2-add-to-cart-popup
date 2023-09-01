33451c065df8c7ff1e0fd828c07e12d023764188
bohdanbatsenko <bohdan.batsenko.vaimo.com>
2023-09-01 13:13:24 +0300

    CR fixes

--- add-cart-popup.js
+++ add-cart-popup.js
@@ -39,58 +39,61 @@ define([
 
         addHandlers() {
             this._on(document, {
-                'ajax:addToCart': this.callPopup.bind(this)
+                'ajax:addToCart': this.subscribeToCartSection.bind(this)
             });
         },
 
-        callPopup(){
-            setTimeout(()=> {
-                const customer = customerData.get('customer')();
-                const cart = customerData.get('cart')();
-                const formattedTotal = Number(cart.subtotalAmount).toFixed();
-                const subtotalRemaining = this.options.freeShippingThreshold - cart.subtotalAmount;
-                let popupWrapper = $('<div class="add-to-cart-modal-popup"/>');
-                let popupContent;
+        subscribeToCartSection() {
+            this.cart = customerData.get('cart').subscribe(this.callPopup.bind(this));
+        },
 
-                if (formattedTotal === '0' ) {
-                    popupContent = `
-                        <p>${$t('Hi, %1').replace('%1', customer.firstname)}</p>
-                        <p>${$t('There are no products in the cart')}</p>
-                    `;
+        callPopup(cart){
 
-                } else if (cart.subtotalAmount > 0 && cart.subtotalAmount < this.options.freeShippingThreshold) {
-                  popupContent = `
-                    <div class="progress-wrapper">
-                        <div class="progress-bar">
-                            <span class="progress-bar-fill" style="width: ${formattedTotal}%;">
-                                <span class="percent-bar">${formattedTotal}%</span>
-                            </span>
-                        </div>
-                    </div>
+            const customer = customerData.get('customer')();
+            const formattedTotal = Number(cart.subtotalAmount).toFixed();
+            const subtotalRemaining = this.options.freeShippingThreshold - cart.subtotalAmount;
+            let popupWrapper = $('<div class="add-to-cart-modal-popup"/>');
+            let popupContent;
+
+            if (formattedTotal === '0' ) {
+                popupContent = `
                     <p>${$t('Hi, %1').replace('%1', customer.firstname)}</p>
-                    <p>You are ${subtotalRemaining}$ away from FREE shipping</p>
-                    <p>${$t('Items in cart: %1').replace('%1', cart.summary_count)}</p>
-                    <p>${$t('Cart subtotal: %1').replace('%1', cart.subtotal)}</p>
-                  `;
+                    <p>${$t('There are no products in the cart')}</p>
+                `;
 
-                } else if (cart.subtotalAmount >= this.options.freeShippingThreshold) {
-                    popupContent = `
-                    <div class="progress-wrapper">
-                        <div class="progress-bar">
-                            <span class="progress-bar-fill" style="width: 100%;">
-                                <span class="percent-bar">100%</span>
-                            </span>
-                        </div>
+            } else if (cart.subtotalAmount > 0 && cart.subtotalAmount < this.options.freeShippingThreshold) {
+              popupContent = `
+                <div class="progress-wrapper">
+                    <div class="progress-bar">
+                        <span class="progress-bar-fill" style="width: ${formattedTotal}%;">
+                            <span class="percent-bar">${formattedTotal}%</span>
+                        </span>
                     </div>
-                    <p>${$t('Hi, %1').replace('%1', customer.firstname)}</p>
-                    <p>${$t(this.options.messageFreeShipping)}</p>
-                    <p>${$t('Items in cart: %1').replace('%1', cart.summary_count)}</p>
-                    <p>${$t('Cart subtotal: %1').replace('%1', formattedTotal)}</p>
-                    `;
-                }
+                </div>
+                <p>${$t('Hi, %1').replace('%1', customer.firstname)}</p>
+                <p>You are ${subtotalRemaining}$ away from FREE shipping</p>
+                <p>${$t('Items in cart: %1').replace('%1', cart.summary_count)}</p>
+                <p>${$t('Cart subtotal: %1').replace('%1', cart.subtotal)}</p>
+              `;
 
-                popupWrapper.html(popupContent).modal(this.options.modalOptions).modal('openModal');
-            }, 1800);
+            } else if (cart.subtotalAmount >= this.options.freeShippingThreshold) {
+                popupContent = `
+                <div class="progress-wrapper">
+                    <div class="progress-bar">
+                        <span class="progress-bar-fill" style="width: 100%;">
+                            <span class="percent-bar">100%</span>
+                        </span>
+                    </div>
+                </div>
+                <p>${$t('Hi, %1').replace('%1', customer.firstname)}</p>
+                <p>${$t(this.options.messageFreeShipping)}</p>
+                <p>${$t('Items in cart: %1').replace('%1', cart.summary_count)}</p>
+                <p>${$t('Cart subtotal: %1').replace('%1', formattedTotal)}</p>
+                `;
+            }
+
+            popupWrapper.html(popupContent).modal(this.options.modalOptions).modal('openModal');
+            this.cart.dispose(); // kill section subscription
         },
 
     });
